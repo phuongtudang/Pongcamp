b2dab094660e5e10bf09948d504a2c7e5816cb03
Phuong <phuongtudang23@gmail.com>
2017-07-19 12:59:26 +0000

    fix alert box

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
+// moment().format();
 // seedDB();
 
 // PASSPORT CONFIGURATION
